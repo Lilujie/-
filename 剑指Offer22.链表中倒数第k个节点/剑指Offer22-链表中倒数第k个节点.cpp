dc5ccc299f12d819d2d3e2389e91979ca80286ce
lilujie <lilujie1211@163.com>
2021-08-25 21:08:18 +0800

    2021-08-25

---
+++ 剑指Offer22.链表中倒数第k个节点/剑指Offer22-链表中倒数第k个节点.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* getKthFromEnd(ListNode* head, int k) {
+        ListNode* fast = head, *slow = head;
+
+        while (k--) {
+            fast = fast->next;
+        }
+
+        while (fast != nullptr) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        return slow;
+    }
+};
--- 剑指Offer24.反转链表/剑指Offer24-反转链表.cpp
+++ 剑指Offer24.反转链表/剑指Offer24-反转链表.cpp
@@ -8,24 +8,15 @@
  */
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) { 
-        // 迭代实现       
-        ListNode* preNode = NULL,* currentNode = head, *tmp;
-        while(currentNode) {
-            tmp = currentNode->next;          
-            currentNode->next = preNode;
-            preNode = currentNode;
-            currentNode = tmp;
-        }
+    ListNode* reverseList(ListNode* head) {
+        ListNode* pre = nullptr, *cur = head, *next = nullptr;
 
-        return preNode;
-        //递归实现
-        // if (head == NULL || head->next == NULL) {
-        //     return head;
-        // }
-        // ListNode *p = reverseList(head->next);
-        // head->next->next = head;
-        // head->next = NULL;
-        // return p;
+        while (cur) {
+            next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
     }
 };
