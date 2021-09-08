29b1e17691e8837ddb536b3c52c58e44dd0ff002
lilujie <lilujie1211@163.com>
2021-09-08 20:40:14 +0800

    2021-09-08

--- 0033.搜索旋转排序数组/0033-搜索旋转排序数组.cpp
+++ 0033.搜索旋转排序数组/0033-搜索旋转排序数组.cpp
@@ -14,13 +14,13 @@ public:
 
             // 根据有序的那个部分确定我们该如何改变二分查找的上下界，
             // 因为我们能够根据有序的那部分判断出 target 在不在这个部分
-            if (nums[0] <= nums[mid]) {
+            if (nums[0] <= nums[mid]) { // 0~mid有序
                 if (target >= nums[0] && target < nums[mid]) {
                     r = mid - 1;
                 } else {
                     l = mid + 1;
                 }
-            } else {
+            } else {  //0~mid 无序，说明另一半有序
                 if (target > nums[mid] && target <= nums[n - 1]) {  // 写成target >= nums[mid + 1]报错
                     l = mid + 1;
                 } else {
